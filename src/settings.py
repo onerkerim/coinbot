bf5925d6c3079a84b44d6b61b7bf5e8f0d06310c
“Oner” <onerkerim@me.com>
2022-03-05 19:12:02 +0300

    first commit

--- src/settings.py
+++ src/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'src.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
+        'NAME': 'd82mj821b20ib6',
         'USER': 'gbkinhdmtmchkv',
         'PASSWORD': 'a210ffae06bd8b55adacf91a8097e787a0d7df7b74ba087ba53311b2bb180006',
         'HOST': 'ec2-23-23-219-25.compute-1.amazonaws.com',
