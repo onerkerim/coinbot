6fe14bbda778dfa70226d820470aea4e81b7fbf9
“Oner” <onerkerim@me.com>
2022-03-06 00:42:29 +0300

    first commit

--- senaryo.py
+++ senaryo.py
@@ -177,26 +177,7 @@ def on_message(ws, message):
         low = float(candle['l'])
         print('close', close, 'high', high, 'low', low, 'kapanış sıra ', closes)
 
-        mail_content = "hello"
-        #The mail addresses and password
-        sender_address = 'pyhonmailgonder@gmail.com'
-        sender_pass = '02120212Oner'
-        receiver_address = 'onerkerim@me.com'
-        #Setup the MIME
-        message = MIMEMultipart()
-        message['From'] = sender_address
-        message['To'] = receiver_address
-        message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
-        #The body and the attachments for the mail
-        message.attach(MIMEText(mail_content, 'plain'))
-        #Create SMTP session for sending the mail
-        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-        session.starttls() #enable security
-        session.login(sender_address, sender_pass) #login with mail_id and password
-        text = message.as_string()
-        session.sendmail(sender_address, receiver_address, text)
-        session.quit()
-        print('Mail Sent')
+
 
         # listelerimize ekliyoruz
         closes.append(close)
@@ -212,6 +193,8 @@ def on_message(ws, message):
                 # buy_order = connection.client.order_market_buy(
                 #     symbol=PAIR,
                 #     quantity=BUY_QUANTITY)
+
+                
             elif color_change and not trend_positive:
                 print('sat sinyali', flush=True)
                 # sell_order = connection.client.order_market_sell(
