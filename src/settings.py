339ba889fa801e1181d80d72572e6890554d6262
“Oner” <onerkerim@me.com>
2022-03-05 15:49:04 +0300

    first commit

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = "/app/static/"
+STATIC_ROOT = "/coinbot/static/"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
